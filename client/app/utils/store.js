b44e867085ffaabd7cb171a43d531ce0725e6dd6
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-10-23 18:36:30 -0500

    created global state in store.js file

---
+++ client/app/utils/store.js
@@ -0,0 +1,71 @@
+import { Store, registerInDevtools } from "pullstate";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth/react-native";
+import { firebase_app, firebase_auth } from "../../firebaseConfig";
+
+export const AuthStore = new Store({
+  isLoggedIn: false,
+  initialized: false,
+  user: null,
+});
+
+const unsub = onAuthStateChanged(firebase_auth, (user) => {
+  console.log("onAuthStateChange", user);
+  AuthStore.update((store) => {
+    store.user = user;
+    store.isLoggedIn = user ? true : false;
+    store.initialized = true;
+  });
+});
+
+export const appSignIn = async (email, password) => {
+  try {
+    const resp = await signInWithEmailAndPassword(firebase_auth, email, password);
+    AuthStore.update((store) => {
+      store.user = resp.user;
+      store.isLoggedIn = resp.user ? true : false;
+    });
+    return { user: firebase_auth.currentUser };
+  } catch (e) {
+    return { error: e };
+  }
+};
+
+export const appSignOut = async () => {
+  try {
+    await signOut(auth);
+    AuthStore.update((store) => {
+      store.user = null;
+      store.isLoggedIn = false;
+    });
+    return { user: null };
+  } catch (e) {
+    return { error: e };
+  }
+};
+
+export const appSignUp = async (email, password, displayName) => {
+  try {
+    // this will trigger onAuthStateChange to update the store..
+    const resp = await createUserWithEmailAndPassword(firebase_auth, email, password);
+
+    // add the displayName
+    await updateProfile(resp.user, { displayName });
+
+    AuthStore.update((store) => {
+      store.user = firebase_auth.currentUser;
+      store.isLoggedIn = true;
+    });
+
+    return { user: firebase_auth.currentUser };
+  } catch (e) {
+    return { error: e };
+  }
+};
+
+registerInDevtools({ AuthStore });
